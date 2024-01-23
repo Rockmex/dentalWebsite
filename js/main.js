9a90966cd95f59efe969786d36d7b810b624c99d
Romer Chia <romerchia@hotmail.com>
2024-01-22 22:55:08 -0500

    pages - Enhanced header; Added "book appointment" button index.html - Added "book appointment" button; Updated format of "view all" text deleted unused js files

--- js/main.js
+++ js/main.js
@@ -47,17 +47,7 @@ Version:	1.1
 		/*====================================
 			Sticky Header JS
 		======================================*/ 
-		jQuery(window).on('scroll', function() {
-			if ($(this).scrollTop() > 100) {
-				$('.header').addClass("sticky");
-			} else {
-				$('.header').removeClass("sticky");
-			}
-		});
-		
-		$('.pro-features .get-pro').on( "click", function(){
-			$('.pro-features').toggleClass('active');
-		});
+
 		
 		/*====================================
 			Search JS
